e38cfb1c9e80b6e37f71318de1d26163aee0f6d6
Phil Kappaz <p.c.kappaz2@gmail.com>
2017-10-02 21:31:29 -0500

    end of class

---
+++ Week-11/Mon-10-2/class.js
@@ -0,0 +1,14 @@
+var student = require("./student.js");
+
+var Class = function(students, number, professor, room, ){
+    this.students = [];
+    this.number = number;
+    this.professor = professor;
+    this.room = room;
+
+this.addstudent = function()
+}
+
+
+
+
---
+++ Week-11/Mon-10-2/student.js
@@ -0,0 +1,7 @@
+var student = function(name, subject, gpa){
+    this.name = name;
+    this.subject = subject;
+    this.gpa = gpa;
+}
+
+module.exports = student;

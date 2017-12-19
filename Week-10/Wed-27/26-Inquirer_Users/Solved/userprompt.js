881c8d581fb00f8a19264db3425313c0fe53d960
PhilipK2 <p.c.kappaz2@gmail.com>
2017-12-18 18:47:16 -0600

    things

--- Week-10/Wed-27/26-Inquirer_Users/Solved/userprompt.js
+++ Week-10/Wed-27/26-Inquirer_Users/Solved/userprompt.js
@@ -1,81 +1,141 @@
-// Create a basic command line Node application using the inquirer package.
-// Your application should ask the user any five questions of your choosing.
-// The question set should include at least one:
-
-//    - Basic input,
-//    - Password,
-//    - List,
-//    - Check-box,
-//    - and Confirm
-
-// Then if a user's password matches a pre-defined password, re-display the data back to the user with some text.
-// See the inquirer GitHub documentation "examples" page if you need help.
-
-// Remember to be creative!
-
-// ========================================================================
-// Load the NPM Package inquirer
-var inquirer = require("inquirer");
-
-// Created a series of questions
-inquirer.prompt([
-
-  {
-    type: "input",
-    name: "name",
-    message: "Who are you???"
-  },
-
-  {
-    type: "list",
-    name: "doingWhat",
-    message: "What are you doing in my house??",
-    choices: ["I made you cookies!", "No lie dude. I'm here to rob you.", "Uh. This is my house... Who are YOU???"]
-  },
-
-  {
-    type: "checkbox",
-    name: "carryingWhat",
-    message: "What are you carrying in your hands??",
-    choices: ["TV", "Slice of Toast", "Butter Knife"]
-  },
-
-  {
-    type: "confirm",
-    name: "canLeave",
-    message: "Can you leave now?"
-  },
-
-  {
-    type: "password",
-    name: "myPassword",
-    message: "Okay fine. You can stay. But only if you say the magic password."
-  }
-
-]).then(function(user) {
-
-  // If the user guesses the password...
-  if (user.myPassword === "myHouse") {
-
-    console.log("==============================================");
-    console.log("");
-    console.log("Well a deal's a deal " + user.name);
-    console.log("You can stay as long as you like.");
-    console.log("Just put down the " + user.carryingWhat.join(" and ") + ". It's kind of freaking me out.");
-    console.log("");
-    console.log("==============================================");
-  }
-
-
-  // If the user doesn't guess the password...
-  else {
-
-    console.log("==============================================");
-    console.log("");
-    console.log("Sorry " + user.name);
-    console.log("I'm calling the cops!");
-    console.log("");
-    console.log("==============================================");
-
-  }
-});
+	// Node module imports needed to run the functions
+	var fs = require("fs"); //reads and writes files
+	var request = require("request");
+	var keys = require("./keys.js");
+	var twitter = require("twitter");
+	var liriArgument = process.argv[2];
+	// ---------------------------------------------------------------------------------------------------------------
+	// Possible commands for this liri app
+	switch (liriArgument) {
+	    case "my-tweets":
+	        myTweets();
+	        break;
+	    case "spotify-this-song":
+	        spotifyThisSong();
+	        break;
+	    case "movie-this":
+	        movieThis();
+	        break;
+	    case "do-what-it-says":
+	        doWhatItSays();
+	        break;
+	        // Instructions displayed in terminal to the user
+	    default:
+	        console.log("\r\n" + "Try typing one of the following commands after 'node liri.js' : " + "\r\n" +
+	            "1. my-tweets 'any twitter name' " + "\r\n" +
+	            "2. spotify-this-song 'any song name' " + "\r\n" +
+	            "3. movie-this 'any movie name' " + "\r\n" +
+	            "4. do-what-it-says." + "\r\n" +
+	            "Be sure to put the movie or song name in quotation marks if it's more than one word.");
+	};
+	// ---------------------------------------------------------------------------------------------------------------
+	// Functions
+	// Movie function, uses the Request module to call the OMDB api
+	function movieThis() {
+	    var movie = process.argv[3];
+	    if (!movie) {
+	        movie = "mr nobody";
+	    }
+	    params = movie
+	    request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function(error, response, body) {
+	        if (!error && response.statusCode == 200) {
+	            var movieObject = JSON.parse(body);
+	            //console.log(movieObject); // Show the text in the terminal
+	            var movieResults =
+	                "------------------------------ begin ------------------------------" + "\r\n"
+	            "Title: " + movieObject.Title + "\r\n" +
+	                "Year: " + movieObject.Year + "\r\n" +
+	                "Imdb Rating: " + movieObject.imdbRating + "\r\n" +
+	                "Country: " + movieObject.Country + "\r\n" +
+	                "Language: " + movieObject.Language + "\r\n" +
+	                "Plot: " + movieObject.Plot + "\r\n" +
+	                "Actors: " + movieObject.Actors + "\r\n" +
+	                "Rotten Tomatoes Rating: " + movieObject.tomatoRating + "\r\n" +
+	                "Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" +
+	                "------------------------------ fin ------------------------------" + "\r\n";
+	            console.log(movieResults);
+	            log(movieResults); // calling log function
+	        } else {
+	            console.log("Error :" + error);
+	            return;
+	        }
+	    });
+	};
+	// Tweet function, uses the Twitter module to call the Twitter api
+	function myTweets() {
+	    var client = new twitter({
+	        consumer_key: keys.twitterKeys.consumer_key,
+	        consumer_secret: keys.twitterKeys.consumer_secret,
+	        access_token_key: keys.twitterKeys.access_token_key,
+	        access_token_secret: keys.twitterKeys.access_token_secret,
+	    });
+	    var twitterUsername = process.argv[3];
+	    if (!twitterUsername) {
+	        twitterUsername = "JahdashaFlagg";
+	    }
+	    params = { screen_name: twitterUsername };
+	    client.get("statuses/user_timeline/", params, function(error, data, response) {
+	        if (!error) {
+	            for (var i = 0; i < data.length; i++) {
+	                //console.log(response); // Show the full response in the terminal
+	                var twitterResults =
+	                    "@" + data[i].user.screen_name + ": " +
+	                    data[i].text + "\r\n" +
+	                    data[i].created_at + "\r\n" +
+	                    "------------------------------ " + i + " ------------------------------" + "\r\n";
+	                console.log(twitterResults);
+	                log(twitterResults); // calling log function
+	            }
+	        } else {
+	            console.log("Error :" + error);
+	            return;
+	        }
+	    });
+	}
+	// Spotify function, uses the Spotify module to call the Spotify api
+	function spotifyThisSong(songName) {
+	    var songName = process.argv[3];
+	    if (!songName) {
+	        songName = "What's my age again";
+	    }
+	    params = songName;
+	    spotify.search({ type: "track", query: params }, function(err, data) {
+	        if (!err) {
+	            var songInfo = data.tracks.items;
+	            for (var i = 0; i < 5; i++) {
+	                if (songInfo[i] != undefined) {
+	                    var spotifyResults =
+	                        "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+	                        "Song: " + songInfo[i].name + "\r\n" +
+	                        "Album the song is from: " + songInfo[i].album.name + "\r\n" +
+	                        "Preview Url: " + songInfo[i].preview_url + "\r\n" +
+	                        "------------------------------ " + i + " ------------------------------" + "\r\n";
+	                    console.log(spotifyResults);
+	                    log(spotifyResults); // calling log function
+	                }
+	            }
+	        } else {
+	            console.log("Error :" + err);
+	            return;
+	        }
+	    });
+	};
+	// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
+	function doWhatItSays() {
+	    fs.readFile("random.txt", "utf8", function(error, data) {
+	        if (!error) {
+	            doWhatItSaysResults = data.split(",");
+	            spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+	        } else {
+	            console.log("Error occurred" + error);
+	        }
+	    });
+	};
+	// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
+	function log(logResults) {
+	    fs.appendFile("log.txt", logResults, (error) => {
+	        if (error) {
+	            throw error;
+	        }
+	    });
+	}
